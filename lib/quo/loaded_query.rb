5490304f014a3593a90c84c133d64394fdc29883
Stephen Ierodiaconou <stevegeek@gmail.com>
2024-09-04 16:54:06 +0200

    Loaded query creation with .wrap instead of overiding initializer

--- lib/quo/loaded_query.rb
+++ lib/quo/loaded_query.rb
@@ -2,6 +2,21 @@
 
 module Quo
   class LoadedQuery < Quo::EagerQuery
-    prop :collection, _Any, writer: false, shadow_check: false
+    def self.wrap(data = nil, props: {}, &block)
+      klass = Class.new(self)
+      if block_given?
+        klass.define_method(:query, &block)
+      elsif data
+        klass.define_method(:query) { data }
+      else
+        raise ArgumentError, "either a query or a block must be provided"
+      end
+      # klass.set_temporary_name = "quo::Wrapper" # Ruby 3.3+
+      klass
+    end
+
+    def loaded?
+      true
+    end
   end
 end
--- test/quo/loaded_query_test.rb
+++ test/quo/loaded_query_test.rb
@@ -4,8 +4,8 @@ require_relative "../test_helper"
 
 class Quo::LoadedQueryTest < ActiveSupport::TestCase
   test "creates a new loaded query object from an array" do
-    q = Quo::LoadedQuery.new(collection: [1, 2, 3])
-    assert_instance_of Quo::LoadedQuery, q
+    q = Quo::LoadedQuery.wrap([1, 2, 3]).new
+    assert_kind_of Quo::LoadedQuery, q
     assert_equal [1, 2, 3], q.to_a
     assert_equal 3, q.results.size
   end
