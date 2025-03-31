1ad2e4b154801d337f4c57128901f97e68f08264
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-03-31 23:43:38 +0200

    Min supported rails version 7

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_31_093940) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_31_093940) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
